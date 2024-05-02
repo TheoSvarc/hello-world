0d3278e93718eb1a2a836f7aa0ffbaddc35fa531
Theodor Švarc <svarc.theodor@gmail.com>
2024-05-02 20:54:14 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,23 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
+        printNumbers();
+        double a = 3;
+        double b = 5;
+        System.out.println(+calculateDiagonal(a, b));
+    }
+
+    private static void printNumbers() {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+    }
+    private static double calculateDiagonal(double a, double b) {
+        double sqrt = Math.sqrt(a * a + b * b);
+        return sqrt;
     }
 }
